0f690064d67d437efaf8215a3c7b4b7f0b8fb27a
danijoo <danijob88@googlemail.com>
2015-10-28 16:20:43 +0100

    button click events

--- app/src/main/java/net/headlezz/appportfolio/MainActivity.java
+++ app/src/main/java/net/headlezz/appportfolio/MainActivity.java
@@ -1,9 +1,11 @@
 package net.headlezz.appportfolio;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,27 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(getString(net.headlezz.appportfolio.R.string.title));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    /**
+     * Main menu button click listener method
+     * @param view Pressed button
+     */
+    public void onClick(View view) {
+        String buttonName = ((Button) view).getText().toString();
+        Toast.makeText(this, String.format(getString(R.string.button_click_message), buttonName), Toast.LENGTH_SHORT).show();
     }
 }
